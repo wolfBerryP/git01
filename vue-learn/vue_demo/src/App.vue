4c90c0923fae008f91e0c4b41746cdd898016647
QuietAsSnow <877510639@qq.com>
2021-08-24 16:00:58 +0800

    des: 1.Vue 案例之列表数据展示

--- vue-learn/vue_demo/src/App.vue
+++ vue-learn/vue_demo/src/App.vue
@@ -1,13 +1,53 @@
 <template>
-
+  <div>
+    <div>
+      <header class="site-header jumbotron">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+              <h1>请发表对Vue的评论</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+    <div class="container">
+      <Add/>
+      <List :comments="comments"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import Add from './components/Add.vue';
+import List from './components/List.vue'
+
 export default {
-  name: "App"
+  data(){
+    return {
+      comments: [
+        {
+          name: '张三',
+          content: '法外狂徒'
+        },
+        {
+          name: '汤姆',
+          content: '...'
+        },
+        {
+          name: '喜羊羊',
+          content: '我走中路'
+        }
+      ]
+    }
+  },
+  components: {
+    Add,
+    List
+  }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
